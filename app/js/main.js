25dbe74a47c2ad41c74c71ff7d511f301a9e6435
peschernuy <peschernuy@gmail.com>
2021-02-06 19:40:47 +0200

    add adaptive about, specisalist, advantages section

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,28 @@ $(function(){
   
 
 
-  $('.services__slider').slick({
-  draggable: true, 
+  $('.services__slider').slick({ 
   slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToScroll: 1,
   infinite: true,
+  draggable: false,
+  rows: 0,
+  responsive:[
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+
+      }
+   },
+    {
+      breakpoint: 660,
+      settings: {
+        slidesToShow: 1,
+
+      }
+    },
+  ]
   
   });
 
@@ -26,7 +43,24 @@ $(function(){
     slidesToScroll: 3,
     infinite: true,
     dots: true,
-    arrows: false
+    arrows: false,
+    
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
 
   });
 
